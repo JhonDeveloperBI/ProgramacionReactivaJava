5403c5bc822eac68b01a4cef0c3c04a43de7e603
John Jaime Ramirez Cortes - Ceiba Software <john.ramirez@ceiba.com.co>
2022-05-06 09:03:40 -0500

    proyect in java

---
+++ webFlux/src/main/java/com/example/webFlux/WebFluxApplication.java
@@ -0,0 +1,26 @@
+package com.example.webFlux;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import reactor.core.publisher.Flux;
+
+@SpringBootApplication
+public class WebFluxApplication  {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebFluxApplication.class, args);
+	}
+/*
+	@Override
+	public void run(String... args) throws Exception {
+		Flux<String> nombres = Flux.just("Jhon","Juan","Pedro","Andrea")
+				.doOnNext(System.out::println); //reference method
+			//	.doOnNext(elemento ->  System.out.println(elemento));
+
+		//subscribe
+		nombres.subscribe();
+	}
+
+ */
+}
