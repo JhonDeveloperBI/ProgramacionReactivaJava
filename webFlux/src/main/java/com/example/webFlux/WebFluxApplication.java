d18497a4a13ce8b638659bf176ab28637ef04c1e
John Jaime Ramirez Cortes - Ceiba Software <john.ramirez@ceiba.com.co>
2022-05-06 09:34:19 -0500

    method subscribe

--- webFlux/src/main/java/com/example/webFlux/WebFluxApplication.java
+++ webFlux/src/main/java/com/example/webFlux/WebFluxApplication.java
@@ -1,12 +1,17 @@
 package com.example.webFlux;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import reactor.core.publisher.Flux;
 
+
+
 @SpringBootApplication
 public class WebFluxApplication implements CommandLineRunner {
+    private  static final Logger log = LoggerFactory.getLogger(WebFluxApplication.class);
 
 	public static void main(String[] args) {
 		SpringApplication.run(WebFluxApplication.class, args);
@@ -14,12 +19,17 @@ public class WebFluxApplication implements CommandLineRunner {
 
 	@Override
 	public void run(String... args) throws Exception {
-		Flux<String> nombres = Flux.just("Jhon","Juan","Pedro","Andrea")
-				.doOnNext(System.out::println); //reference method
-			//	.doOnNext(elemento ->  System.out.println(elemento));
+		Flux<String> nombres = Flux.just("Jhon","Juan","Pedro","","Andrea")
+				.doOnNext(e ->{
+					if( e.isEmpty()){
+						throw new RuntimeException("Los nombres no pueden ser vacios");
+					}
+					System.out.println(e);
+				});
 
 		//subscribe
-		nombres.subscribe();
+		nombres.subscribe(e -> log.info(e),
+				error -> log.error( error.getMessage())); // observador, consumidor
 	}
 
 
