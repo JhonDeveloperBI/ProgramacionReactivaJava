86572a4ad2325ccd396629534f1b8dd95e3dd30f
John Jaime Ramirez Cortes - Ceiba Software <john.ramirez@ceiba.com.co>
2022-05-11 15:37:33 -0500

    import data of example and operator FlatMap

--- spring-boot-webflux-web/spring-boot-webflux-web/src/main/java/com/bolsadeideas/springboot/webfluxweb/springbootwebfluxweb/SpringBootWebfluxWebApplication.java
+++ spring-boot-webflux-web/spring-boot-webflux-web/src/main/java/com/bolsadeideas/springboot/webfluxweb/springbootwebfluxweb/SpringBootWebfluxWebApplication.java
@@ -1,13 +1,42 @@
 package com.bolsadeideas.springboot.webfluxweb.springbootwebfluxweb;
 
+import com.bolsadeideas.springboot.webfluxweb.springbootwebfluxweb.models.dao.ProductoDao;
+import com.bolsadeideas.springboot.webfluxweb.springbootwebfluxweb.models.documents.Producto;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import reactor.core.publisher.Flux;
 
 @SpringBootApplication
-public class SpringBootWebfluxWebApplication {
+public class SpringBootWebfluxWebApplication implements CommandLineRunner {
+
+	private static final Logger log = LoggerFactory.getLogger(SpringBootWebfluxWebApplication.class);
+
+	@Autowired
+	private ProductoDao dao;
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringBootWebfluxWebApplication.class, args);
 	}
 
+	@Override
+	public void run(String... args) throws Exception {
+		Flux.just(new Producto("TV panasonic",550.36),
+				new Producto("Playstation",600.00),
+				new Producto("Playstation 1 ",600.00),
+				new Producto("Playstation 2",600.00),
+				new Producto("Playstation 3",600.00),
+				new Producto("Playstation 4",600.00),
+				new Producto("Playstation 5",600.00),
+				new Producto("Playstation 6",600.00),
+				new Producto("Playstation 7",600.00)
+		).flatMap(producto -> dao.save(producto))
+				.subscribe(producto -> log.info("Insert:" + producto.getId()+
+						" " + producto.getNombre() ));
+
+
+	}
 }
--- spring-boot-webflux-web/spring-boot-webflux-web/src/main/java/com/bolsadeideas/springboot/webfluxweb/springbootwebfluxweb/models/documents/Producto.java
+++ spring-boot-webflux-web/spring-boot-webflux-web/src/main/java/com/bolsadeideas/springboot/webfluxweb/springbootwebfluxweb/models/documents/Producto.java
@@ -2,6 +2,7 @@ package com.bolsadeideas.springboot.webfluxweb.springbootwebfluxweb.models.docum
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
@@ -20,5 +21,11 @@ public class Producto {
     private  String nombre;
     private  Double precio;
     private Date createAt;
+
+
+    public Producto(String nombre, Double precio){
+        this.nombre = nombre;
+        this.precio = precio;
+    }
 
 }
