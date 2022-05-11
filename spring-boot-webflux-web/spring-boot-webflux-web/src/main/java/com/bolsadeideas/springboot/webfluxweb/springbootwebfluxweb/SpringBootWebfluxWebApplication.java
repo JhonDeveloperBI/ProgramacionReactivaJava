6ef38c16bbcb454c627f23f0a42ff781078fb93d
John Jaime Ramirez Cortes - Ceiba Software <john.ramirez@ceiba.com.co>
2022-05-11 10:29:01 -0500

    init project

---
+++ spring-boot-webflux-web/spring-boot-webflux-web/src/main/java/com/bolsadeideas/springboot/webfluxweb/springbootwebfluxweb/SpringBootWebfluxWebApplication.java
@@ -0,0 +1,13 @@
+package com.bolsadeideas.springboot.webfluxweb.springbootwebfluxweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootWebfluxWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootWebfluxWebApplication.class, args);
+	}
+
+}
---
+++ spring-boot-webflux-web/spring-boot-webflux-web/src/test/java/com/bolsadeideas/springboot/webfluxweb/springbootwebfluxweb/SpringBootWebfluxWebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bolsadeideas.springboot.webfluxweb.springbootwebfluxweb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootWebfluxWebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
