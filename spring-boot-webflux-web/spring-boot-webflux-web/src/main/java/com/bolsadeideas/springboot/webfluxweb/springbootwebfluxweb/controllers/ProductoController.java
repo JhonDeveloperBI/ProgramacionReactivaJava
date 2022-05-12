4942812d1da2f6ef3bfc0b41f3e938e431c4b772
John Jaime Ramirez Cortes - Ceiba Software <john.ramirez@ceiba.com.co>
2022-05-12 08:23:56 -0500

    suscribiendo other observator and modificando stream reactivo

--- spring-boot-webflux-web/spring-boot-webflux-web/src/main/java/com/bolsadeideas/springboot/webfluxweb/springbootwebfluxweb/controllers/ProductoController.java
+++ spring-boot-webflux-web/spring-boot-webflux-web/src/main/java/com/bolsadeideas/springboot/webfluxweb/springbootwebfluxweb/controllers/ProductoController.java
@@ -2,6 +2,8 @@ package com.bolsadeideas.springboot.webfluxweb.springbootwebfluxweb.controllers;
 
 import com.bolsadeideas.springboot.webfluxweb.springbootwebfluxweb.models.dao.ProductoDao;
 import com.bolsadeideas.springboot.webfluxweb.springbootwebfluxweb.models.documents.Producto;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -11,12 +13,19 @@ import reactor.core.publisher.Flux;
 @Controller
 public class ProductoController {
 
+    private static  final Logger log = LoggerFactory.getLogger(ProductoController.class);
+
     @Autowired
     private ProductoDao dao;
 
     @GetMapping({"/listar","/"})
     public String listar(Model model){
-        Flux<Producto> productos = dao.findAll();
+        Flux<Producto> productos = dao.findAll().map(producto -> {
+            producto.setNombre(producto.getNombre().toUpperCase());
+            return producto;
+        });
+        productos.subscribe(prod -> log.info(prod.getNombre()));
+
         model.addAttribute("productos",productos);
         model.addAttribute("titulo","Listado de productos");
 
