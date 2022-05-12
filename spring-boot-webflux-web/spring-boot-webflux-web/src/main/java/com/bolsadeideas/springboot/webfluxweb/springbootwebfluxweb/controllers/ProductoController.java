84736edf8fa31313d657c5988faf0fcf803143bd
John Jaime Ramirez Cortes - Ceiba Software <john.ramirez@ceiba.com.co>
2022-05-12 08:32:31 -0500

    mode reactive data driven for manipulate the backpresure

--- spring-boot-webflux-web/spring-boot-webflux-web/src/main/java/com/bolsadeideas/springboot/webfluxweb/springbootwebfluxweb/controllers/ProductoController.java
+++ spring-boot-webflux-web/spring-boot-webflux-web/src/main/java/com/bolsadeideas/springboot/webfluxweb/springbootwebfluxweb/controllers/ProductoController.java
@@ -8,7 +8,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.thymeleaf.spring5.context.webflux.ReactiveDataDriverContextVariable;
 import reactor.core.publisher.Flux;
+
+import java.time.Duration;
 
 @Controller
 public class ProductoController {
@@ -27,6 +30,21 @@ public class ProductoController {
         productos.subscribe(prod -> log.info(prod.getNombre()));
 
         model.addAttribute("productos",productos);
+        model.addAttribute("titulo","Listado de productos");
+
+        return "listar";
+    }
+
+    @GetMapping("/listar-datadriver")
+    public String listarDataDriver(Model model){
+        Flux<Producto> productos = dao.findAll().map(producto -> {
+            producto.setNombre(producto.getNombre().toUpperCase());
+            return producto;
+        }).delayElements(Duration.ofSeconds(1));
+
+        productos.subscribe(prod -> log.info(prod.getNombre()));
+
+        model.addAttribute("productos", new ReactiveDataDriverContextVariable(productos,2) );
         model.addAttribute("titulo","Listado de productos");
 
         return "listar";
