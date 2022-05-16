7c7a3e574cc668993700507889766db04d326804
John Jaime Ramirez Cortes - Ceiba Software <john.ramirez@ceiba.com.co>
2022-05-16 09:35:02 -0500

    added categorias of example and operator thenMany

--- spring-boot-webflux-web/spring-boot-webflux-web/src/main/java/com/bolsadeideas/springboot/webfluxweb/springbootwebfluxweb/SpringBootWebfluxWebApplication.java
+++ spring-boot-webflux-web/spring-boot-webflux-web/src/main/java/com/bolsadeideas/springboot/webfluxweb/springbootwebfluxweb/SpringBootWebfluxWebApplication.java
@@ -1,7 +1,8 @@
 package com.bolsadeideas.springboot.webfluxweb.springbootwebfluxweb;
 
-import com.bolsadeideas.springboot.webfluxweb.springbootwebfluxweb.models.dao.ProductoDao;
+import com.bolsadeideas.springboot.webfluxweb.springbootwebfluxweb.models.documents.Categoria;
 import com.bolsadeideas.springboot.webfluxweb.springbootwebfluxweb.models.documents.Producto;
+import com.bolsadeideas.springboot.webfluxweb.springbootwebfluxweb.models.services.ProductoServiceImpl;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -19,7 +20,7 @@ public class SpringBootWebfluxWebApplication implements CommandLineRunner {
 	private static final Logger log = LoggerFactory.getLogger(SpringBootWebfluxWebApplication.class);
 
 	@Autowired
-	private ProductoDao dao;
+	private ProductoServiceImpl productoService;
 
 	@Autowired
 	private ReactiveMongoTemplate mongoTemplate;
@@ -31,20 +32,33 @@ public class SpringBootWebfluxWebApplication implements CommandLineRunner {
 	@Override
 	public void run(String... args) throws Exception {
 		mongoTemplate.dropCollection("productos").subscribe();
+		mongoTemplate.dropCollection("categorias").subscribe();
 
-		Flux.just(new Producto("TV panasonic",550.36),
-				new Producto("Playstation",600.00),
-				new Producto("Playstation 1 ",600.00),
-				new Producto("Playstation 2",600.00),
-				new Producto("Playstation 3",600.00),
-				new Producto("Playstation 4",600.00),
-				new Producto("Playstation 5",600.00),
-				new Producto("Playstation 6",600.00),
-				new Producto("Playstation 7",600.00)
-		).flatMap(producto ->{
-			producto.setCreateAt(new Date());
-			return dao.save(producto);
-		})
+		Categoria electronico = new Categoria("Electrónico");
+		Categoria deporte = new Categoria("Deporte");
+		Categoria computacion = new Categoria("Computación");
+		Categoria muebles = new Categoria("Muebles");
+
+		Flux.just(electronico,deporte,computacion,muebles).flatMap(categoria ->{
+					return productoService.save(categoria);
+				})
+				.doOnNext(categoria -> log.info("Insert:" + categoria.getId()+
+						" " + categoria.getNombre() )).thenMany(
+
+						Flux.just(new Producto("TV panasonic",550.36, electronico),
+								new Producto("Sony Camara HD",600.00,electronico),
+								new Producto("Apple ipod ",600.00,electronico),
+								new Producto("Sony Notebook",600.00,computacion),
+								new Producto("Hewlett Packard Multifuncional",600.00,computacion),
+								new Producto("Bicicleta",600.00,deporte),
+								new Producto("Notebook Omen 17",600.00,computacion),
+								new Producto("Mica Comada 5 cajones",600.00,muebles),
+								new Producto("TV Sony Bravia OLED 4k",600.00,electronico)
+						).flatMap(producto ->{
+							producto.setCreateAt(new Date());
+							return productoService.save(producto);
+						})
+				)
 				.subscribe(producto -> log.info("Insert:" + producto.getId()+
 						" " + producto.getNombre() ));
 
