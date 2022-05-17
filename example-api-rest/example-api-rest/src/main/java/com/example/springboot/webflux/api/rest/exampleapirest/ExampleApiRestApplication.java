8db65523a4926954ca71be92150a537b6f96b059
John Jaime Ramirez Cortes - Ceiba Software <john.ramirez@ceiba.com.co>
2022-05-17 07:57:30 -0500

    --- proyecto nuevo ---

---
+++ example-api-rest/example-api-rest/src/main/java/com/example/springboot/webflux/api/rest/exampleapirest/ExampleApiRestApplication.java
@@ -0,0 +1,13 @@
+package com.example.springboot.webflux.api.rest.exampleapirest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExampleApiRestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExampleApiRestApplication.class, args);
+	}
+
+}
---
+++ example-api-rest/example-api-rest/src/test/java/com/example/springboot/webflux/api/rest/exampleapirest/ExampleApiRestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springboot.webflux.api.rest.exampleapirest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExampleApiRestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
