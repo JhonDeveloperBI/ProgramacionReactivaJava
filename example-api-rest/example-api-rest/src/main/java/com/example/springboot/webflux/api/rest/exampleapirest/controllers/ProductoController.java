851bc2b0a286981516a2733593478e6bc42214a0
John Jaime Ramirez Cortes - Ceiba Software <john.ramirez@ceiba.com.co>
2022-05-17 09:05:47 -0500

    implement handler Get for list in restController

---
+++ example-api-rest/example-api-rest/src/main/java/com/example/springboot/webflux/api/rest/exampleapirest/controllers/ProductoController.java
@@ -0,0 +1,30 @@
+package com.example.springboot.webflux.api.rest.exampleapirest.controllers;
+
+import com.example.springboot.webflux.api.rest.exampleapirest.models.documents.Producto;
+import com.example.springboot.webflux.api.rest.exampleapirest.models.services.ProductoService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
+@RestController
+@RequestMapping("api/productos")
+public class ProductoController {
+
+    @Autowired
+    private ProductoService service;
+
+    @GetMapping
+    public Mono<ResponseEntity<Flux<Producto>>> lista(){
+        return Mono.just(
+                ResponseEntity.ok( )
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .body(service.findAll())
+        );
+    }
+
+}
