4d4867ab90cb36245d1373f44256a0b8bef38165
John Jaime Ramirez Cortes - Ceiba Software <john.ramirez@ceiba.com.co>
2022-05-17 15:34:36 -0500

    added put for update

--- example-api-rest/example-api-rest/src/main/java/com/example/springboot/webflux/api/rest/exampleapirest/handler/ProductoHandler.java
+++ example-api-rest/example-api-rest/src/main/java/com/example/springboot/webflux/api/rest/exampleapirest/handler/ProductoHandler.java
@@ -9,6 +9,9 @@ import org.springframework.web.reactive.function.server.ServerRequest;
 import org.springframework.web.reactive.function.server.ServerResponse;
 import reactor.core.publisher.Mono;
 
+import java.net.URI;
+import java.util.Date;
+
 import static org.springframework.web.reactive.function.BodyInserters.fromValue;
 
 @Component
@@ -28,7 +31,37 @@ public class ProductoHandler {
                 .contentType(MediaType.APPLICATION_JSON)
                 .body(fromValue(p)))
                 .switchIfEmpty(ServerResponse.notFound().build());
-
     }
 
+    public Mono<ServerResponse> crear(ServerRequest request){
+        Mono<Producto> producto = request.bodyToMono(Producto.class);
+
+        return producto.flatMap(p -> {
+            if(p.getCreateAt() == null){
+                p.setCreateAt(new Date());
+            }
+            return service.save(p);
+        }).flatMap( p -> ServerResponse
+                .created(URI.create("/api/v2/productos".concat(p.getId())))
+                .contentType(MediaType.APPLICATION_JSON)
+                .body(fromValue(p)));
+    }
+
+    public Mono<ServerResponse> editar(ServerRequest request){
+        Mono<Producto> producto = request.bodyToMono(Producto.class);
+        String id = request.pathVariable("id");
+
+        Mono<Producto> productoDb = service.findById(id);
+        return productoDb.zipWith(producto,(db,req) ->{
+            db.setNombre(req.getNombre());
+            db.setPrecio(req.getPrecio());
+            db.setCategoria(req.getCategoria());
+
+            return db;
+        } ).flatMap( p -> ServerResponse.created(URI.create("/api/v2/productos".concat(p.getId())) )
+                .contentType(MediaType.APPLICATION_JSON)
+                .body(service.save(p),Producto.class)
+                .switchIfEmpty(ServerResponse.notFound().build())
+        );
+    }
 }
