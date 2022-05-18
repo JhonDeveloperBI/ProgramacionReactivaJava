45dded84b9ab610450217122fd18330dc2ccd1b1
John Jaime Ramirez Cortes - Ceiba Software <john.ramirez@ceiba.com.co>
2022-05-18 08:22:53 -0500

    list with consumer

--- example-api-rest/example-api-rest/src/test/java/com/example/springboot/webflux/api/rest/exampleapirest/ExampleApiRestApplicationTests.java
+++ example-api-rest/example-api-rest/src/test/java/com/example/springboot/webflux/api/rest/exampleapirest/ExampleApiRestApplicationTests.java
@@ -1,11 +1,14 @@
 package com.example.springboot.webflux.api.rest.exampleapirest;
 
 import com.example.springboot.webflux.api.rest.exampleapirest.models.documents.Producto;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.reactive.server.WebTestClient;
+
+import java.util.List;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class ExampleApiRestApplicationTests {
@@ -23,6 +26,26 @@ class ExampleApiRestApplicationTests {
 				.expectHeader().contentType(MediaType.APPLICATION_JSON)
 				.expectBodyList(Producto.class)
 				.hasSize(9);
+	}
+
+	@Test
+	public void listar2Test() {
+		client.get()
+				.uri("/api/v2/productos")
+				.accept(MediaType.APPLICATION_JSON)
+				.exchange()
+				.expectStatus().isOk()
+				.expectHeader().contentType(MediaType.APPLICATION_JSON)
+				.expectBodyList(Producto.class)
+				.consumeWith(response ->{
+					List<Producto> productos = response.getResponseBody();
+					productos.forEach( p ->{
+						System.out.println(p.getNombre());
+					});
+
+					Assertions.assertTrue(productos.size() > 0);
+				});
+
 	}
 
 }
