a2a592cd05a714375feb0c3fa71efe54e372ef16
John Jaime Ramirez Cortes - Ceiba Software <john.ramirez@ceiba.com.co>
2022-05-18 08:15:14 -0500

    test endpoint listar

--- example-api-rest/example-api-rest/src/test/java/com/example/springboot/webflux/api/rest/exampleapirest/ExampleApiRestApplicationTests.java
+++ example-api-rest/example-api-rest/src/test/java/com/example/springboot/webflux/api/rest/exampleapirest/ExampleApiRestApplicationTests.java
@@ -1,13 +1,28 @@
 package com.example.springboot.webflux.api.rest.exampleapirest;
 
+import com.example.springboot.webflux.api.rest.exampleapirest.models.documents.Producto;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.reactive.server.WebTestClient;
 
-@SpringBootTest
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class ExampleApiRestApplicationTests {
 
+	@Autowired
+	private WebTestClient client;
+
 	@Test
-	void contextLoads() {
+	public void listarTest() {
+		client.get()
+				.uri("/api/v2/productos")
+				.accept(MediaType.APPLICATION_JSON)
+				.exchange()
+				.expectStatus().isOk()
+				.expectHeader().contentType(MediaType.APPLICATION_JSON)
+				.expectBodyList(Producto.class)
+				.hasSize(9);
 	}
 
 }
